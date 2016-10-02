4fb1beb91aa53341c18be868fbf54fe1ad668b1e
Demian Rodriguez <demian85@gmail.com>
2016-10-02 10:47:07 -0300

    Update ESlint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,5 +2,11 @@ module.exports = {
     "extends": "airbnb-base",
     "plugins": [
         "import"
-    ]
+    ],
+    "rules": {
+      "no-use-before-define": 0,
+      "prefer-arrow-callback": 0,
+      "func-names": 0,
+      "no-console": 0
+    }
 };
